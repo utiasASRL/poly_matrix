7b7c68c46e4d93e4ac03f87293152bd2d1d7cd82
Frederike Duembgen <frederike.duembgen@gmail.com>
2024-01-20 14:23:45 -0500

    Update least squares

--- poly_matrix/least_squares_problem.py
+++ poly_matrix/least_squares_problem.py
@@ -28,6 +28,7 @@ class LeastSquaresProblem(object):
     def get_Q(self):
         if self.Q is None:
             self.Q = self.B.transpose().multiply(self.B)
+            self.Q.symmetric = True
         return self.Q
 
     def add_residual(self, res_dict: dict):
@@ -39,25 +40,3 @@ class LeastSquaresProblem(object):
             self.B[self.m, key] += val
         self.m += 1
         return
-
-        # old implementation directly constructs Q.
-        for diag, val in res_dict.items():
-            # forbid 1-dimensional arrays cause they are ambiguous.
-            assert np.ndim(val) in [
-                0,
-                2,
-            ]
-            if np.ndim(val) == 0:
-                self[diag, diag] += val**2
-            else:
-                self[diag, diag] += val @ val.T
-
-        for off_diag_pair in itertools.combinations(res_dict.items(), 2):
-            dict0, dict1 = off_diag_pair
-
-            if np.ndim(dict1[1]) > 0:
-                new_val = dict0[1] * dict1[1].T
-            else:
-                new_val = dict0[1] * dict1[1]
-            # new value is an array:
-            self[dict0[0], dict1[0]] += new_val
